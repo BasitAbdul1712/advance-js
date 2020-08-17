47cf073432465f04f261eb8d34729abe2a67370c
BasitAbdul1712 <67551884+BasitAbdul1712@users.noreply.github.com>
2020-08-17 15:23:44 +0200

    double vs tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,14 @@
+//**double vs String
+// == dile kunta string r kunta int check korbena same holei true dekhabe
+// === same whether sring or int na hole same 2  holeo false dekhabe
+
+// 0 and bollean false 2 ta negative vale == dile true dekhabe but === false dekhabe coz ita int and boolean
+
+const first =  2;
+const second = "2";
+if (first === second) {
+    console.log("true");
+}
+else{
+    console.log("false");
+}
